1959bf3c0cce2de8153fe435ed3326062c9fed66
Marina <marinalaguna2004@gmail.com>
2023-10-05 13:54:17 +0200

    modificacion archivos html y css

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ function cargarCabecera(dest){
 function retirarDinero(){
     
 }
+
+function validarDineroRetirar(){
+    saldo = document.getElementById('saldo').value
+    retirar = document.getElementById('retirar').value
+
+    if (saldo > retirar){
+        return
+    }else{
+        return
+    }
+}
