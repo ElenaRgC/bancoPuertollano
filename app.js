d0ebe73531ce470404b1194ba4ebb7a3b69f84d2
Elena Rodríguez <95099947+ElenaRgC@users.noreply.github.com>
2023-10-05 17:48:05 +0200

    Creada función validarTarjeta()

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-let botonCliente = document.getElementById('boton-cliente');
-let botonTarjeta = document.getElementById('boton-tarjeta');
-let parrafoCliente = document.getElementById('mensaje-cliente');
+let botonCliente = document.getElementById("boton-cliente");
+let botonTarjeta = document.getElementById("boton-tarjeta");
+let parrafoCliente = document.getElementById("mensaje-cliente");
 
-function cargarDatos(){
+function cargarDatos() {
     /* Carga los datos de index.html */
-    document.getElementById('nombre').value = persona.nombre;
-    document.getElementById('apellido1').value = persona.apellido1;
-    document.getElementById('apellido2').value = persona.apellido2;
-    document.getElementById('nacionalidad').value = persona.nacionalidad;
+    document.getElementById("nombre").value = persona.nombre;
+    document.getElementById("apellido1").value = persona.apellido1;
+    document.getElementById("apellido2").value = persona.apellido2;
+    document.getElementById("nacionalidad").value = persona.nacionalidad;
 
     /* Carga el menú */
-    let menu = document.getElementById('menu').innerHTML;
+    let menu = document.getElementById("menu").innerHTML;
 }
 
-function cargarCabecera(dest){  
- document.getElementById(dest).innerHTML = '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>' 
+function cargarCabecera(dest) {
+    document.getElementById(dest).innerHTML =
+        '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>';
 }
 
 // OBJETOS ---------------------------
 
 let persona = {
-    'nombre': 'Elena',
-    'apellido1': 'Rodríguez',
-    'apellido2': 'Calderón',
-    'nacionalidad': 'Española'
+    nombre: "Elena",
+    apellido1: "Rodríguez",
+    apellido2: "Calderón",
+    nacionalidad: "Española",
 };
 
 let tarjetas = [
     {
-        'numero': '1234 12345 123456',
-        'cvv': '123',
-        'activa': true,
+        numero: "1234 12345 123456",
+        cvv: "123",
+        activa: true,
     },
     {
-        'numero': '1234 12345 123457',
-        'cvv': '321',
-        'activa': false,
+        numero: "1234 12345 123457",
+        cvv: "321",
+        activa: false,
     },
 ];
 
@@ -43,53 +44,47 @@ crearTabla();
 
 // LISTENERS --------------------------
 
-botonCliente.addEventListener('click', function() {
+botonCliente.addEventListener("click", function () {
     modificarDatos(validarDatos());
 });
 
-botonTarjeta.addEventListener('click', function() {
-    guardarTarjeta();
+botonTarjeta.addEventListener("click", function () {
+    guardarTarjeta(validarTarjeta());
     crearTabla();
 });
 
 // FUNCIONES ----------------------------
 
 function modificarDatos(datos) {
-
     if (datos) {
-        persona.nombre = datos['Nombre'];
-        persona.apellido1 = datos['Primer apellido'];
-        persona.apellido2 = datos['Segundo apellido'];
-        persona.nacionalidad = datos['Nacionalidad'];
+        persona.nombre = datos["Nombre"];
+        persona.apellido1 = datos["Primer apellido"];
+        persona.apellido2 = datos["Segundo apellido"];
+        persona.nacionalidad = datos["Nacionalidad"];
 
         parrafoCliente.innerText = "Datos guardados correctamente.";
-        parrafoCliente.setAttribute('class', 'correcto');
+        parrafoCliente.setAttribute("class", "correcto");
     }
 }
 
-function guardarTarjeta() {
-
-    let nuevaTarjeta = {
-        'numero':document.getElementById('numero').value,
-        'cvv': document.getElementById('cvv').value,
-        'activa': document.getElementById('activa').checked,
-    };
-
-    tarjetas.push(nuevaTarjeta);
+function guardarTarjeta(tarjeta) {
+    if (tarjeta) {
+        tarjetas.push(tarjeta);
+    }
 }
 
 function crearTabla() {
-    let tabla = document.getElementById('tabla');
+    let tabla = document.getElementById("tabla");
     tabla.innerHTML = "";
 
     for (let tarjeta of tarjetas) {
-        let tr = document.createElement('tr');
+        let tr = document.createElement("tr");
         for (let dato in tarjeta) {
-            let td = document.createElement('td');
+            let td = document.createElement("td");
             td.textContent = tarjeta[dato];
             tr.appendChild(td);
         }
-        tabla.appendChild(tr);       
+        tabla.appendChild(tr);
     }
 }
 
@@ -101,30 +96,56 @@ function validarDatos() {
     let mensaje = "";
 
     let datosNuevos = {
-        'Nombre': document.getElementById('nombre').value.trim(),
-        'Primer apellido': document.getElementById('apellido1').value.trim(),
-        'Segundo apellido': document.getElementById('apellido2').value.trim(),
-        'Nacionalidad': document.getElementById('nacionalidad').value.trim()
+        Nombre: document.getElementById("nombre").value.trim(),
+        "Primer apellido": document.getElementById("apellido1").value.trim(),
+        "Segundo apellido": document.getElementById("apellido2").value.trim(),
+        Nacionalidad: document.getElementById("nacionalidad").value.trim(),
     };
 
     for (let dato in datosNuevos) {
-        if (dato != 'Nacionalidad') {
-            if(!patronNyA.test(datosNuevos[dato])) {
+        if (dato != "Nacionalidad") {
+            if (!patronNyA.test(datosNuevos[dato])) {
                 mensaje += dato + " debe tener entre 3 y 20 caracteres. \n";
             }
         } else {
-            if(!patronNacionalidad.test(datosNuevos[dato])) {
+            if (!patronNacionalidad.test(datosNuevos[dato])) {
                 mensaje += dato + " debe tener entre 3 y 15 caracteres. \n";
             }
         }
     }
 
     if (mensaje === "") {
-            return datosNuevos;
+        return datosNuevos;
     } else {
-            parrafoCliente.innerText = mensaje;
-            parrafoCliente.setAttribute('class', 'error');
-            return false;
+        parrafoCliente.innerText = mensaje;
+        parrafoCliente.setAttribute("class", "error");
+        return false;
     }
-    
+}
+
+function validarTarjeta() {
+    let patronCvv = /^\d{3}$/;
+    let patronTarjeta = /^\d{4}\s\d{5}\s\d{6}$/;
+    let mensaje = "";
+
+    let tarjetaNueva = {
+        numero: document.getElementById("numero").value.trim(),
+        cvv: document.getElementById("cvv").value.trim(),
+        activa: document.getElementById("activa").checked,
+    };
+
+    if (!patronTarjeta.test(tarjetaNueva["numero"])) {
+        mensaje += "El número de la tarjeta no sigue el siguiente formato: \n";
+        mensaje += "XXXX XXXXX XXXXXX \n";
+    }
+
+    if (!patronCvv.test(tarjetaNueva["cvv"])) {
+        mensaje += "El CVV sólo puede estar compuesto de 3 números.";
+    }
+
+    if (mensaje == "") {
+        return tarjetaNueva;
+    } else {
+        return false;
+    }
 }
